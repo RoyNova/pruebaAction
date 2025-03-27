0b174a879672c968ceba5efc3e77fc50b4118731
RoyNova <146909415+RoyNova@users.noreply.github.com>
2025-03-26 18:57:06 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, SonarQube!");
+        System.out.println("Hello, SonarQube! and trivy");
     }
 }
